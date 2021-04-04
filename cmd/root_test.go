fd41fa26ba602e37f55e00098ea4eddc41f3f133
mamau <mopckou_pak48rus@mail.ru>
2021-04-04 23:48:36 +0300

    тесты

---
+++ cmd/root_test.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestGetRunnableCommand(t *testing.T) {
+	os.Args = []string{"starter", "php", "-v"}
+	if cmd := getRunnableCommand(); cmd != "php" {
+		t.Errorf("runnable command must be %q", "php")
+	}
+
+	os.Args = []string{"starter"}
+	if cmd := getRunnableCommand(); cmd != "" {
+		t.Error("runnable command must be empty")
+	}
+}
+
+func TestGetAvailableCommands(t *testing.T) {
+	ac := getAvailableCommands()
+	var availableCommands []string
+	for _, v := range rootCmd.Commands() {
+		availableCommands = append(availableCommands, v.Name())
+	}
+
+	if strings.Join(ac, " ") != strings.Join(availableCommands, " ") {
+		t.Errorf("available command expected %q\n got %q\n", strings.Join(ac, " "), strings.Join(availableCommands, " "))
+	}
+}
--- cmd/service.go
+++ cmd/service.go
@@ -1,8 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/gookit/color"
 	"github.com/mamau/starter/entity"
 	"github.com/mamau/starter/libs"
 	"github.com/mamau/starter/services"
@@ -15,11 +14,11 @@ import (
 var serviceCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 2 {
-			fmt.Println("You should pass args")
+			color.Red.Printf("You should pass args")
 			return
 		}
-
 		serviceName := args[0]
+		color.Cyan.Printf("Start %s\n", serviceName)
 
 		config := config.GetConfig()
 		s := config.GetService(serviceName)
