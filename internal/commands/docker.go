90f29d761a1bc36b4a5bc944fe0ba46d8a29b18d
mamau <mopckou_pak48rus@mail.ru>
2022-02-21 01:03:55 +0200

    добавлена команда которая дает информацию о других командах

--- internal/commands/docker.go
+++ internal/commands/docker.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"github.com/gookit/color"
 	"github.com/spf13/cobra"
+	"satellite/internal/entity"
 	"satellite/internal/entity/docker"
 	"satellite/internal/informator"
 )
@@ -13,10 +14,23 @@ var dockerCmd = &cobra.Command{
 	Long:    "позволяет показать какие опции можно использовать в конфиге докер секции",
 	Example: "./sat docker run",
 	Run: func(cmd *cobra.Command, args []string) {
-		exec := docker.Exec{}
-		info := informator.NewInformator(exec)
+		//TODO: аргумент проверять!
+		info := informator.NewInformator(getCommand(args[0]))
 		for i, v := range info.GetAll() {
 			color.Cyan.Printf("%-20s%s\n", i, v)
 		}
 	},
+}
+
+func getCommand(slug string) entity.Runner {
+	switch slug {
+	case "exec":
+		return docker.Exec{}
+	case "run":
+		return docker.Run{}
+	case "pull":
+		return docker.Pull{}
+	default:
+		return nil
+	}
 }
